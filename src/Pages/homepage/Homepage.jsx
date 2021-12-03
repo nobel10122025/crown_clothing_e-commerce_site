693f87ee3e4c3ea7f09d811e2ba402d8b6f0f470
unknown <nobel10122025@gmail.com>
2021-12-03 16:13:20 +0000

    added and styled home page

---
+++ src/Pages/homepage/Homepage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Directory from '../../Components/directory/directory'
+import './Homepage.scss'
+function Homepage() {
+    return (
+        <div className="homepage">
+            <Directory />        
+        </div>
+    )
+}
+
+export default Homepage
