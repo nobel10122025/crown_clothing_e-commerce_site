8630fa9680722299d06ffd6224c7b66ba83ebd9e
unknown <nobel10122025@gmail.com>
2021-12-05 17:48:10 +0000

    added and styled header

--- src/App.js
+++ src/App.js
@@ -3,15 +3,19 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Homepage from './Pages/homepage/Homepage'
 import Shoppage from './Pages/Shoppage/Shoppage'
+import Header from './Components/header/Header'
 
 function App() {
   return (
+    <>
     <Router>
+      <Header />
       <Routes>
         <Route path="/" caseSensitive={false} element={<Homepage />} />
         <Route path="/Shoppage" caseSensitive={false} element={<Shoppage />} />
       </Routes>
     </Router>
+    </>
   );
 }
 
