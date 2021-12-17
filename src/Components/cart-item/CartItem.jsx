e282dc53ee486cf7689ca97a8c5713dbe4300d74
unknown <nobel10122025@gmail.com>
2021-12-17 13:43:54 +0530

    resloved img issue in cartItem

--- src/Components/cart-item/CartItem.jsx
+++ src/Components/cart-item/CartItem.jsx
@@ -4,7 +4,7 @@ import './CartItem.scss'
 function CartItem({ item : {imageUrl ,name , price , quantity}}) {
     return (
         <div className='cart-item'>
-            <img scr = {imageUrl} alt ="item"/>
+            <div class="image" style = {{backgroundImage : `url(${imageUrl})`}} />
             <div className='item-details'>
             <span className='name'>{name}</span>
             <span className='price'>
